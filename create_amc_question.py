ceea6ba14686665d3234e3a5b0df24863f315d67
Jonathan Gross <azaghalofbelegost@gmail.com>
2014-01-16 12:41:00 -0700

    Changed to using grid manager for layout, and fixed a label typo.

--- create_amc_question.py
+++ create_amc_question.py
@@ -8,23 +8,19 @@ class App:
     frame.pack()
 
     top_label = Label(frame, text="New AMC question")
-    top_label.pack()
+    top_label.grid(row=0, columnspan=3)
 
-    label_frame = Frame(frame)
-    label_frame.pack()
     self.question_label = StringVar()
-    label_label = Label(label_frame, text="Question label")
-    label_label.pack(side=LEFT)
-    label_entry = Entry(label_frame, textvariable=self.question_label)
-    label_entry.pack(side=LEFT)
+    label_label = Label(frame, text="Question label:")
+    label_label.grid(row=1, sticky=W)
+    label_entry = Entry(frame, textvariable=self.question_label)
+    label_entry.grid(row=1, column=1, sticky=W)
 
-    question_frame = Frame(frame)
-    question_frame.pack()
     self.question_text = StringVar()
-    question_label = Label(question_frame, text="Questioni text")
-    question_label.pack(side=LEFT)
-    question_entry = Entry(question_frame, textvariable=self.question_text)
-    question_entry.pack(side=LEFT)
+    question_label = Label(frame, text="Question text:")
+    question_label.grid(row=2, sticky=W)
+    question_entry = Entry(frame, textvariable=self.question_text)
+    question_entry.grid(row=2, column=1, sticky=W)
 
     label_entry.focus_set()
 
@@ -33,23 +29,23 @@ class App:
     ans_labels = []
     ans_entries = []
     ans_radios = []
-    ans_frames = []
 
-    for n in range(4):
-      ans_frames.append(Frame(frame))
-      ans_frames[n].pack()
+    rows_before_ans = 3
+    self.answer_count = 4
+
+    for n in range(self.answer_count):
       self.ans_texts.append(StringVar())
-      ans_labels.append(Label(ans_frames[n], text="Answer " + str(n + 1)))
-      ans_labels[n].pack(side=LEFT)
-      ans_entries.append(Entry(ans_frames[n], textvariable=self.ans_texts[n]))
-      ans_entries[n].pack(side=LEFT)
-      ans_radios.append(Radiobutton(ans_frames[n], variable=self.correct_ans,
+      ans_labels.append(Label(frame, text="Answer " + str(n + 1) + ":"))
+      ans_labels[n].grid(row=rows_before_ans + n, sticky=W)
+      ans_entries.append(Entry(frame, textvariable=self.ans_texts[n]))
+      ans_entries[n].grid(row=rows_before_ans + n, column=1, sticky=W)
+      ans_radios.append(Radiobutton(frame, variable=self.correct_ans,
         value=n))
-      ans_radios[n].pack(side=LEFT)
+      ans_radios[n].grid(row=rows_before_ans + n, column=2, sticky=W)
 
     write_button = Button(frame, text="Generate question",
       command=self.print_to_terminal)
-    write_button.pack()
+    write_button.grid(row=rows_before_ans + self.answer_count, columnspan=3)
 
   def print_to_terminal(self):
 
@@ -57,9 +53,9 @@ class App:
     print("  " + self.question_text.get())
     print("  \\begin{choices}")
     marks = ["correct" if n == self.correct_ans.get() else "wrong" for n in
-      range(4)]
+      range(self.answer_count)]
 
-    for n in range(4):
+    for n in range(self.answer_count):
       print("    \\" + marks[n] + "choice{" + self.ans_texts[n].get() + "}")
 
     print("  \\end{choices}")
