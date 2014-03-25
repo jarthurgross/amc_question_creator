824f615837eff8b25f923a74d8c68496e999b9a9
Jonathan A. Gross <azaghalofbelegost@gmail.com>
2014-03-24 19:40:24 -0600

    Added some comments.

--- amc_mvc.py
+++ amc_mvc.py
@@ -37,6 +37,9 @@ from amc_question_dialogs import CreateQuestionDialog, EditQuestionDialog
 
 
 class Observable:
+    '''A mutable element of the model that can notify the controller when
+    modified
+    '''
     def __init__(self, initialValue=None):
         self.data = initialValue
         self.callbacks = {}
@@ -66,6 +69,8 @@ class Model:
     def __init__(self):
         self.myQuestions = Observable([])
         self.saveFile = Observable(os.getcwd() + "/amc_questions_output.tex")
+        self.papertypes = ["letterpaper", "a4paper"]
+        self.papertype = self.papertypes[0]
 
     def addQuestion(self, question):
         self.myQuestions.set(self.myQuestions.get() +
@@ -84,56 +89,107 @@ class Model:
     def setFile(self, filename):
         self.saveFile.set(filename)
 
+    def writeTest(self):
+        questions_string = "% Created with AMC Question Creator"
+        papertype = self.papertypes[0]
+        questions_string += "\n\\documentclass[" + papertype + "]{article}"
+        questions = self.myQuestions.get()
+        for question in questions:
+            indent = ""
+            questions_string += "\n\n" + indent + "\\begin{question}{" + \
+                question.get_label() + "}"
+            indent += "  "
+            questions_string += "\n" + indent + question.get_question()
+            questions_string += "\n" + indent + "\\begin{choices}"
+            answers = question.get_answers()
+            marks = ["correct" if n == question.get_correct() else "wrong" for
+                n in range(len(answers))]
+            indent += "  "
+            for n in range(len(answers)):
+              questions_string += \
+                "\n" + indent + "\\" + marks[n] + "choice{" + answers[n] + "}"
+            indent = indent[:-2]
+            questions_string += "\n" + indent + "\\end{choices}"
+            indent = indent[:-2]
+            questions_string += "\n" + indent + "\\end{question}"
+
+        return questions_string
+
 
 class View(tk.Toplevel):
-    def __init__(self, master):
+    def __init__(self, master, papertypes):
         tk.Toplevel.__init__(self, master)
         self.protocol('WM_DELETE_WINDOW', self.master.destroy)
 
-        tk.Label(self, text='AMC questions').grid(row=0, columnspan=3)
+        cur_row = 0
+
+        tk.Label(self, text='AMC questions').grid(row=cur_row, columnspan=3)
+
+        cur_row += 1
+
+        self.paperSize = tk.StringVar(self)
+        self.paperSize.set(papertypes[0])
+        self.paperOptionMenu = tk.OptionMenu(self, self.paperSize,
+            *papertypes)
+        self.paperOptionMenu.grid(row=cur_row, column=1, sticky='w')
+
+        cur_row += 1
 
         self.newButton = tk.Button(self, text="New question")
-        self.newButton.grid(row=1, sticky='w')
+        self.newButton.grid(row=cur_row, sticky='w')
 
         self.editButton = tk.Button(self, text="Edit question")
-        self.editButton.grid(row=1, column=1, sticky='w')
+        self.editButton.grid(row=cur_row, column=1, sticky='w')
 
         self.deleteButton = tk.Button(self, text="Delete question")
-        self.deleteButton.grid(row=1, column=2, sticky='w')
+        self.deleteButton.grid(row=cur_row, column=2, sticky='w')
+
+        cur_row += 1
 
         self.questionListbox = tk.Listbox(self)
-        self.questionListbox.grid(row=2, columnspan=3)
+        self.questionListbox.grid(row=cur_row, columnspan=3)
+
+        cur_row += 1
 
         self.saveLabel = tk.Label(self)
-        self.saveLabel.grid(row=3, columnspan=3, sticky='w')
+        self.saveLabel.grid(row=cur_row, columnspan=3, sticky='w')
+
+        cur_row += 1
 
         self.selectFileButton = tk.Button(self, text="Choose file")
-        self.selectFileButton.grid(row=4, column=1, sticky='w')
+        self.selectFileButton.grid(row=cur_row, column=1, sticky='w')
 
         self.saveFileButton = tk.Button(self, text="Save")
-        self.saveFileButton.grid(row=4, column=2, sticky='w')
+        self.saveFileButton.grid(row=cur_row, column=2, sticky='w')
 
 
     def RefreshQuestions(self, questions):
         self.questionListbox.delete(0, 'end')
         for question in questions:
             self.questionListbox.insert('end', question.get_label())
-        
+
 
 class Controller:
     def __init__(self, root):
         self.model = Model()
         self.model.myQuestions.addCallback(self.QuestionsChanged)
         self.model.saveFile.addCallback(self.FileChanged)
-        self.view = View(root)
+        self.view = View(root, self.model.papertypes)
+        self.ConfigureView()
+        self.QuestionsChanged(self.model.myQuestions.get())
+        self.FileChanged(self.model.saveFile.get())
+
+    def ConfigureView(self):
+        self.view.paperOptionMenu.config(command=self.UpdatePaperType)
         self.view.newButton.config(command=self.AddQuestion)
         self.view.deleteButton.config(command=self.RemoveQuestion)
         self.view.editButton.config(command=self.EditQuestion)
         self.view.selectFileButton.config(command=self.SelectFile)
         self.view.saveFileButton.config(command=self.SaveFile)
-        self.QuestionsChanged(self.model.myQuestions.get())
-        self.FileChanged(self.model.saveFile.get())
         
+    def UpdatePaperType(self, papertype):
+        self.model.papertype = papertype
+
     def AddQuestion(self):
         d = CreateQuestionDialog(self.view, title="Create Question")
         if d.new_question:
@@ -163,7 +219,7 @@ class Controller:
             self.model.setFile(filename)
 
     def SaveFile(self):
-        questionsString = self.WriteTest()
+        questionsString = self.model.writeTest()
         with open(self.model.saveFile.get(), 'w') as f:
             f.write(questionsString)
         f.closed
@@ -173,25 +229,6 @@ class Controller:
 
     def FileChanged(self, filename):
         self.view.saveLabel.config(text=filename)
-
-    def WriteTest(self):
-        questions_string = "% Created with AMC Question Creator"
-        questions = self.model.myQuestions.get()
-        for question in questions:
-            questions_string += "\n\n\\begin{question}{" + \
-                question.get_label() + "}"
-            questions_string += "\n  " + question.get_question()
-            questions_string += "\n  \\begin{choices}"
-            answers = question.get_answers()
-            marks = ["correct" if n == question.get_correct() else "wrong" for
-                n in range(len(answers))]
-            for n in range(len(answers)):
-              questions_string += \
-                "\n    \\" + marks[n] + "choice{" + answers[n] + "}"
-            questions_string += "\n  \\end{choices}"
-            questions_string += "\n\\end{question}"
-
-        return questions_string
 
 
 if __name__ == '__main__':
